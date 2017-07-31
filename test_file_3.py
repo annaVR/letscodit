8333cfcd1c648c22306c35b6f21463667a6a1d9c
annaVR <anna@kirsanova.name>
2017-07-31 16:46:22 -0700

    study: bug in maximize_window

--- test_file_3.py
+++ test_file_3.py
@@ -3,24 +3,25 @@ __author__ = 'anna'
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import os
 
 url = 'https://letskodeit.teachable.com/'
+
+# driver_location = '/Users/anna/bin/chromedriver'
+# os.environ['webdriver.chrome.driver'] = driver_location
+# driver = webdriver.Chrome(driver_location)
+
 driver = webdriver.Firefox()
 driver.get(url)
 driver.implicitly_wait(4)
-driver.maximize_window()
-time.sleep(3)
-driver = webdriver.Firefox()
-driver.get(url)
-driver.implicitly_wait(4)
-driver.maximize_window()
+# driver.maximize_window()
 time.sleep(3)
 
-# driver.find_element(By.XPATH, '//href="/sign_in"').click()
-# time.sleep(2)
-# driver.find_element(By.ID, 'user_email').send_keys('test@email.com')
-# driver.find_element(By.ID, 'user_password').send_keys('abcabc')
-# time.sleep(2)
-# driver.find_element(By.NAME, 'commit').click()
-# time.sleep(3)
+driver.find_element(By.XPATH, '//a[@href="/sign_in"]').click()
+time.sleep(3)
+driver.find_element(By.ID, 'user_email').send_keys('test@email.com')
+driver.find_element(By.ID, 'user_password').send_keys('abcabc')
+time.sleep(2)
+driver.find_element(By.NAME, 'commit').click()
+time.sleep(3)
 driver.quit()
